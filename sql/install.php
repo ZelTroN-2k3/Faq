2c5054b45697b43bbbf7f31a2f89632b4f13f2b1
ZelTroN-2k3 <zeltron_workgroups@hotmail.com>
2021-07-10 11:18:32 +0200

    modifiant le fichier faq/sql/install.php
    J'ai corrigé le souci en modifiant le fichier faq/sql/install.php. J'ai remplacé les 2 lignes qui concernent les colonnes "created" et "modified". Voici les bonnes valeurs :
    sa:
    `created` datetime DEFAULT CURRENT_TIMESTAMP ,
    `modified` datetime DEFAULT CURRENT_TIMESTAMP ,
    par:
    `created` datetime NOT NULL,
    `modified` datetime NOT NULL,

--- sql/install.php
+++ sql/install.php
@@ -9,14 +9,14 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'faq` (
 
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'faq_lang` (
   `id_qa` int(11) NOT NULL auto_increment,
-  `id_lang` int(11) NOT NULL ,
+  `id_lang` int(11) NOT NULL,
   `question` text NOT NULL,
   `answer` text NOT NULL,
-  `created` datetime DEFAULT CURRENT_TIMESTAMP ,
-  `modified` datetime DEFAULT CURRENT_TIMESTAMP ,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
   PRIMARY KEY (`id_qa`,`id_lang`)
 ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8';
 
 $sql[] = 'INSERT INTO `' . _DB_PREFIX_ . 'linksmenutop` (id_shop, new_window) VALUES(
   '.(int)Context::getContext()->shop->id.',0)';
-?>
+?>
